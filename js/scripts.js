7978e36311d2090746a6a859726e4e5259d1b7b2
Leah Nelson and Maarja Laineste <maarja@>
2016-06-20 10:24:49 -0700

    add form input, jumbotron, and list and adopt options

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-/*This file is for your custom js.  All yours*/
+$(document).ready(function() {
+  $("#list").click(function() {
+    $(".new-animal").show();
+  });
+  $("#new-animal").submit(function(event) {
+    event.preventDefault();
+
+
+    var name = $("#name").val();
+    var sex = $("#male-female").val();
+    var type = $("#type").val();
+    var age = parseInt($("#age").val());
+    var description = $("#description").val();
+    var image = $("#image").val();
+
+    $("p#test").append("<img src=" + image + ">");
+    // var newPet = new Pet
+  });
+  $("#adopt").click(function() {
+    $(".new-animal").hide();
+  });
+});
